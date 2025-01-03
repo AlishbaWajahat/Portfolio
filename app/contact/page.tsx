37c71089c80b3fa63a53fed3f469bab7e9fea601
Alishba Wajahat <alishbawajahat867@gmail.com>
2025-01-02 18:06:20 -0800

    fixed a bug

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -25,7 +25,7 @@ export default function page() {
                         {/* <!-- Message Field --> */}
                         <div>
                             <label className="block text-left text-gray-700 font-medium mb-4">Your Message</label>
-                            <textarea id="message" name="message" required rows="6" class="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500 focus:outline-none"></textarea>
+                            <textarea id="message" name="message" required rows={6} className="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500 focus:outline-none"></textarea>
                         </div>
 
                         {/* <!-- Submit Button --> */}
