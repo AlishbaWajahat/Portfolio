2fcf73bd8ccfe7ceabb0656696c93ef371d19ce3
Alishba Wajahat <alishbawajahat867@gmail.com>
2025-01-02 18:16:01 -0800

    fixed some issues

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
       </div>
 
       {/* Content Section */}
-      <div className="relative z-10 flex flex-col items-start justify-center lg:px-0 px-6 py-24  md:py-40 max-w-3xl md:max-w-5xl lg:ml-32 mx-auto my-16">
+      <div className="relative z-10 flex flex-col items-start justify-center gap-8 lg:px-0 px-6 py-24 md:py-40 max-w-3xl md:max-w-5xl lg:ml-32 mx-auto my-16 md:my-o">
         {/* Typing Effect Text */}
         <p className=" text-3xl sm:text-6xl font-extrabold text-white font-kanit leading-tight ml-2">
           <span className={`text-white typing-effect flex flex-wrap`}>
@@ -36,7 +36,7 @@ export default function Home() {
         {/* CTA or Links (Optional) */}
         <div className="mt-10 flex space-x-6 text-center">
           <a
-            href="#contact"
+            href="/contact"
             className="bg-blue-500 text-white px-8 py-4 rounded-lg hover:bg-blue-600 transition-all duration-300 text-xl sm:text-2xl"
           >
             Get in Touch
